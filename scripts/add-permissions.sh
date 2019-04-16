513bec112726ef3796a7e3df04e566b65b434233
Jack Williams <jackw53519@gmail.com>
2019-04-16 23:38:48 +0100

    Change permissions method of appspec

--- scripts/add-permissions.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT_DIR=`dirname $DIR`
-sudo chmod 777 -R $ROOT_DIR
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /var/server/
-./scripts/add-permissions.sh
 java -jar CombinedService.jar &
